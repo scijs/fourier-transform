5994469ec8070059f6ed53479bb889ab58ce78e6
Dmitry <df.creative@gmail.com>
2016-05-13 12:29:24 -0400

    Add aureoom-js-fft

--- research.js
+++ research.js
@@ -18,17 +18,54 @@ for (var i = 0; i < N; i++) {
 var max = 10e2;
 
 
-var STFT = require('stft');
-test('stft', function (done) {
-	var count = 0;
-	var onfreq = function () {
-		count++;
-		if (count >= 10) done();
-	};
-	var stft = STFT(1, N, onfreq);
-	for (var i = 0; i < max; i++ ) {
-		stft(real);
+test('aureooms-js-fft', function () {
+	var fft = require( "aureooms-js-fft" );
+	var array = require( "aureooms-js-array" ) ;
+	var random = require( "aureooms-js-random" ) ;
+	var number = require( "aureooms-js-number" ) ;
+	var complex = require( "aureooms-js-complex" ) ;
+
+	var kernel = complex.cartesian.kernel.compile( number , "i" ) ;
+	var cartesian = complex.cartesian.array.compile( kernel ) ;
+
+	var $ = fft.compile( cartesian ) ;
+	var _ = cartesian.complex ;
+
+	var n = N;//Math.pow( 2 , 10 ) | 0 ;
+	var l = Math.log2( n ) | 0 ;
+	var m = n >>> 1 ;
+
+	var u = array.alloc( n ) ;
+	var v = array.alloc( n ) ;
+	var w = array.alloc( n ) ;
+
+	for ( var i = 0 ; i < n ; ++i ) {
+		u[i] = _( random.randfloat( -1000 , 1000 ) , random.randfloat( -1000 , 1000 ) ) ;
 	}
+
+	array.copy( u , 0 , n , w , 0 ) ;
+
+	test('run 10 ×', function () {
+		for (var i = 0; i < max/10; i++ ) {
+			$.fft( l , m , u , 0 , n  , v , 0 , n ) ;
+		}
+	});
+});
+
+
+var STFT = require('stft');
+test('stft', function () {
+	test('run', function (done) {
+		var count = 0;
+		var onfreq = function () {
+			count++;
+			if (count >= 10) done();
+		};
+		var stft = STFT(1, N, onfreq);
+		for (var i = 0; i < max; i++ ) {
+			stft(real);
+		}
+	});
 });
 
 
